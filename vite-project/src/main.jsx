ffeb28fb51e338b679fc8680324187c6aa85ccf1
LeoAlonso01 <127887838+LeoAlonso01@users.noreply.github.com>
2024-10-02 13:31:35 -0600

    change of client

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -5,14 +5,14 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import './index.css'
 
 
-/* const clientId = import.meta.env.VITE_APP_CLIENT_ID;
-const domain = import.meta.env.VITE_APP_DOMAIN; */
+const clientId = import.meta.env.VITE_APP_CLIENID;
+const domain = import.meta.env.VITE_APP_DOMINIO;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Auth0Provider
-    domain="dev-85kjznb2qyq6rnuv.us.auth0.com"
-    clientId="byEkx5Y5l4U7MC8w8cI2R0umcDHnc8ME"
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
